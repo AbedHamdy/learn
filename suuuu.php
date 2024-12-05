5c70827a09ce9056ec496066ba7de603fa5747fe
abed <abdoezzat960@gmail.com>
2024-12-05 10:45:28 -0800

    update name

--- suuuu.php
+++ suuuu.php
@@ -10,7 +10,7 @@
     <thead>
         <tr>
             <th>id</th>
-            <th></th>
+            <th>name</th>
             <th>last_name</th>
             <th>email</th>
             <th>gender</th>
